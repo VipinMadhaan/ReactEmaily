ad5beaada8a7b24f5d30451a669d375901288d90
VipinKumarMadhaan <vipin.madhaan@gmail.com>
2019-10-06 16:06:44 +0530

    changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({ Hi : 'Vipin Madhaan' });
 });
 
 const PORT = process.env.PORT || 5000;
