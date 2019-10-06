6623e631b183242dabccd715e3a0cd95abdca3b2
VipinKumarMadhaan <vipin.madhaan@gmail.com>
2019-10-06 18:52:51 +0530

    Third commit

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+        console.log(accessToken);
+    })
+);
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google',{
+        scope: ['profile','email']
+    })
+);
+/*app.get('/', (req, res) => {
     res.send({ Hi : 'Vipin Madhaan' });
-});
+});*/
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
