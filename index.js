ee1ec1e660e9f192ad599edbbc1a3f658234bbc7
VipinKumarMadhaan <vipin.madhaan@gmail.com>
2019-10-09 16:59:53 +0530

    added auth

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    },
-    accessToken => {
-        console.log(accessToken);
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
     })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get(
-    '/auth/google',
-    passport.authenticate('google',{
-        scope: ['profile','email']
-    })
-);
-/*app.get('/', (req, res) => {
-    res.send({ Hi : 'Vipin Madhaan' });
-});*/
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
+/*app.get('/', (req, res) => {
+    res.send({ Hi : 'Vipin Madhaan' });
+});*/
